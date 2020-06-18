90b1a8fc825c135e15e1e759165c39fd70a70935
Kosmostars7403 <3699818@mail.ru>
2020-06-18 13:07:38 +0300

    Setup media files serving

--- places/models.py
+++ places/models.py
@@ -10,6 +10,9 @@ class Place(models.Model):
     def __str__(self):
         return self.title
 
+class Image(models.Model):
+    title = models.ForeignKey(Place, on_delete=models.CASCADE)
+    file = models.ImageField(upload_to='places_images')
 
-
-
+    def __str__(self):
+        return '{} {}'.format(self.pk, self.title)
