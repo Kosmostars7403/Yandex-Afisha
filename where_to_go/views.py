f149ab937caaae7a566bad3c5249d44aaabc09c0
Kosmostars7403 <3699818@mail.ru>
2020-06-18 14:05:53 +0300

    Load GeoJson

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,8 +1,30 @@
 from django.http import HttpResponse
 from django.template import loader
+from places.models import Place, Image
 
 def index(request):
     template = loader.get_template('index.html')
-    context = {}
+    places = Place.objects.all()
+    context = {'places_geojson':
+        {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    }
+    for place in places:
+        place_geo = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.lng, place.lat]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": "moscow_legends",
+                "detailsUrl": "{% static 'places/moscow_legends.json' %}"
+            }
+        }
+        context['places_geojson']['features'].append(place_geo)
+
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
